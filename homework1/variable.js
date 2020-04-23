474e324167b34ea1cc9c37cd09821d7e23a2fb77
kimminji122258 <kimminji122258@gmail.com>
2020-04-24 00:52:05 +0900

    first seminar homework

---
+++ homework1/variable.js
@@ -0,0 +1,42 @@
+/* 변수 재선언 실습 */
+
+var v = '선언됨';
+var v = '재선언됨';
+console.log("v : ", v);
+
+let l = '선언됨';
+let l = '재선언됨';
+console.log("l : ", l); //재선언 불가_에러
+
+const c = '선언됨';
+const c = '재선언됨';
+console.log("c : ", c); //재선언 불가_에러
+
+
+
+/* 2. 변수 재할당 실습 */
+
+// var vv = '선언됨';
+// vv = '재할당됨';
+// console.log("vv : ", vv);
+
+// let ll = '선언됨';
+// ll = '재할당됨';
+// console.log("ll : ", ll);
+
+// const cc = '선언됨';
+// cc = '재할당됨';
+// console.log("cc : ", cc); //재할당 불가_에러
+
+
+
+/* 3. 변수 초기화 실습 */
+
+// var vv;
+// console.log("vv : ", vv);
+
+// let ll;
+// console.log("ll : ", ll);
+
+// const cc;
+// console.log("cc : ", cc); //초기화 필요_에러
